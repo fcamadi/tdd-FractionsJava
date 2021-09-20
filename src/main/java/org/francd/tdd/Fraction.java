cf434244024d8bd96eb403e565992fe1083dbf43
Fran Camacho <fcamadi@gmail.com>
2021-09-20 22:18:53 +0200

    feat: added test zero_plus_zero
    Added class Fraction so it compiles

---
+++ src/main/java/org/francd/tdd/Fraction.java
@@ -0,0 +1,16 @@
+package org.francd.tdd;
+
+public class Fraction {
+
+
+    public Fraction(int integerValue) {
+    }
+
+    public Fraction plus(Fraction that) {
+        return this;
+    }
+
+    public int intValue() {
+        return 0;
+    }
+}
---
+++ src/test/java/org/francd/tdd/AddFractionsTest.java
@@ -0,0 +1,15 @@
+package org.francd.tdd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AddFractionsTest {
+
+    @Test
+    void zero_plus_zero()  {
+
+        Fraction num = new Fraction(0).plus(new Fraction(0));
+        assertEquals(0,num.intValue());
+    }
+}
