e0afbdc5029910a2e53ccf853f577ac9725554f5
Fran Camacho <fcamadi@gmail.com>
2021-09-21 22:05:04 +0200

    fix: renamed test to non_trivial_but_common_denominator

--- src/test/java/org/francd/tdd/AddFractionsTest.java
+++ src/test/java/org/francd/tdd/AddFractionsTest.java
@@ -37,7 +37,7 @@ public class AddFractionsTest {
     }
 
     @Test
-    void non_trivial_denominator() {
+    void non_trivial_but_common_denominator() {
         final Fraction sum =  new Fraction(1,5).plus((new Fraction(2,5)));
         assertEquals(3,sum.getNumerator());
         assertEquals(5,sum.getDenominator());
