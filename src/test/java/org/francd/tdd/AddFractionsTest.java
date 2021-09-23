3a42ac6e0134fb77d02f1b95b6d4db2663b968b5
Fran Camacho <fcamadi@gmail.com>
2021-09-23 22:17:12 +0200

    refactor: we only use now the equals method

--- src/test/java/org/francd/tdd/AddFractionsTest.java
+++ src/test/java/org/francd/tdd/AddFractionsTest.java
@@ -8,26 +8,25 @@ public class AddFractionsTest {
 
     @Test
     void zero_plus_zero()  {
-
-        assertEquals(0, new Fraction(0).plus(new Fraction(0)).intValue());
+        assertEquals(new Fraction(0), new Fraction(0).plus(new Fraction(0)));
     }
 
     @Test
     void nonzero_plus_zero()  {
 
-        assertEquals(3, new Fraction(3).plus(new Fraction(0)).intValue());
+        assertEquals(new Fraction(3), new Fraction(3).plus(new Fraction(0)));
     }
 
     @Test
     void zero_plus_nonzero()  {
 
-        assertEquals(5, new Fraction(0).plus(new Fraction(5)).intValue());
+        assertEquals(new Fraction(5), new Fraction(0).plus(new Fraction(5)));
     }
 
     @Test
     void nonnegative_nonzero_operands()  {
-
-        assertEquals(7, new Fraction(3).plus(new Fraction(4)).intValue());
+        //assertEquals(7, new Fraction(3).plus(new Fraction(4)).intValue());
+        assertEquals(new Fraction(7), new Fraction(3).plus(new Fraction(4)));
     }
 
     @Test
@@ -38,10 +37,9 @@ public class AddFractionsTest {
 
     @Test
     void non_trivial_but_common_denominator() {
-        final Fraction sum =  new Fraction(1,5).plus((new Fraction(2,5)));
-//        assertEquals(3,sum.getNumerator());     First we add the new assertion.
-//        assertEquals(5,sum.getDenominator());   If the test still passses: remove the old ones (here I comment them out)
-        assertEquals(new Fraction(3,5),sum);
+        //assertEquals(3,sum.getNumerator());     First we add the new assertion.
+        //assertEquals(5,sum.getDenominator());   If the test still passses: remove the old ones (here I comment them out)
+        assertEquals(new Fraction(3,5), new Fraction(1,5).plus((new Fraction(2,5))));
     }
 
 
