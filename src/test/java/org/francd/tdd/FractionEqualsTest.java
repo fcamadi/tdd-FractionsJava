8b1a54499f5958925cfc81b31ff509ca3d799d55
Fran Camacho <fcamadi@gmail.com>
2021-09-23 22:00:07 +0200

    feat: more tests to be sure equals is correctly implemented

--- src/test/java/org/francd/tdd/FractionEqualsTest.java
+++ src/test/java/org/francd/tdd/FractionEqualsTest.java
@@ -3,6 +3,7 @@ package org.francd.tdd;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class FractionEqualsTest {
 
@@ -11,4 +12,26 @@ public class FractionEqualsTest {
     void same_numerator_and_denominator() {
         assertEquals(new Fraction(3,5), new Fraction(3,5));
     }
+
+    @Test
+    void different_numerators() {
+        assertNotEquals(new Fraction(1,5), new Fraction(2,5));
+    }
+
+    @Test
+    void different_denominators() {
+        assertNotEquals(new Fraction(2,3), new Fraction(2,5));
+    }
+
+    @Test
+    void whole_number_equals_same_fraction() {
+        assertEquals(new Fraction(5,1), new Fraction(5));
+    }
+
+    @Test
+    void whole_number_not_equal_to_different_whole_number() {
+        assertNotEquals(new Fraction(6), new Fraction(5));
+    }
+
+
 }
