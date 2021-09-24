841ab7cec5996a95895fa2b5f11abceb3111c0e0
Fran Camacho <fcamadi@gmail.com>
2021-09-24 20:57:26 +0200

    fix: added convention that GCD should be always positive

--- src/test/java/org/francd/tdd/GreatestCommonDivisorTest.java
+++ src/test/java/org/francd/tdd/GreatestCommonDivisorTest.java
@@ -11,14 +11,14 @@ public class GreatestCommonDivisorTest {
     void reflexive() {
         assertEquals(1, gcd(1,1));
         assertEquals(2, gcd(2,2));
-        assertEquals(-1, gcd(-1,-1));
+        assertEquals(1, gcd(-1,-1));
     }
 
     @Test
     void relatively_prime() {
         assertEquals(1, gcd(2,3));
         assertEquals(1, gcd(4,7));
-        assertEquals(-1, gcd(-2,-3));
+        assertEquals(1, gcd(-2,-3));
     }
 
     @Test
@@ -31,7 +31,7 @@ public class GreatestCommonDivisorTest {
     void common_factor() {
         assertEquals(2, gcd(6,8));
         assertEquals(7, gcd(49,315));
-        assertEquals(-4, gcd(-24,-28));
+        assertEquals(4, gcd(-24,-28));
     }
 
     @Test
@@ -39,7 +39,7 @@ public class GreatestCommonDivisorTest {
         //not sure if this behaviour is fine ..
         //(GCD should not be always positive?)
         assertEquals(4, gcd(-24,28));
-        assertEquals(-4, gcd(24,-28));
+        assertEquals(4, gcd(24,-28));
     }
 
 
@@ -50,6 +50,6 @@ public class GreatestCommonDivisorTest {
             b = a % t;
             a = t;
         }
-        return a;
+        return Math.abs(a);
     }
 }
