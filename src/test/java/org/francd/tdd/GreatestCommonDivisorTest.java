ad52db3bde4771fba214d1e8ce741055d7d8d1cf
Fran Camacho <fcamadi@gmail.com>
2021-09-24 20:55:35 +0200

    feat: "explored GCD algorithm from Wikipedia; found an anoying aspect of its contract"

--- src/test/java/org/francd/tdd/GreatestCommonDivisorTest.java
+++ src/test/java/org/francd/tdd/GreatestCommonDivisorTest.java
@@ -8,9 +8,40 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class GreatestCommonDivisorTest {
 
     @Test
-    void one_and_one() {
+    void reflexive() {
         assertEquals(1, gcd(1,1));
+        assertEquals(2, gcd(2,2));
+        assertEquals(-1, gcd(-1,-1));
     }
+
+    @Test
+    void relatively_prime() {
+        assertEquals(1, gcd(2,3));
+        assertEquals(1, gcd(4,7));
+        assertEquals(-1, gcd(-2,-3));
+    }
+
+    @Test
+    void one_is_multiple_of_the_other() {
+        assertEquals(3, gcd(3,9));
+        assertEquals(5, gcd(5,30));
+    }
+
+    @Test
+    void common_factor() {
+        assertEquals(2, gcd(6,8));
+        assertEquals(7, gcd(49,315));
+        assertEquals(-4, gcd(-24,-28));
+    }
+
+    @Test
+    void negatives() {
+        //not sure if this behaviour is fine ..
+        //(GCD should not be always positive?)
+        assertEquals(4, gcd(-24,28));
+        assertEquals(-4, gcd(24,-28));
+    }
+
 
     private int gcd(int a, int b) {
         //taken from wikipedia (iterative version)
